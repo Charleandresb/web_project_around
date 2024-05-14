812a488893a69d44f26cf81320ea36592b865d36
Charles <carlos.arte91@gmail.com>
2024-05-13 21:26:34 -0400

    crear tarjetas js

--- README.md
+++ README.md
@@ -1,17 +1,34 @@
 # Tripleten web_project_around
 
-Este proyecto se trata de un viaje al rededor de los Estados Unidos. Donde
+Este proyecto se trata de un viaje alrededor de los Estados Unidos. Donde
 nuestro explorador Jacques Cousteau comparte algunas de sus fotografías de
 montañas, parques y lagos, de esta y otras partes del mundo también.
 
-Esta vez se implementó un perfil editable a traves del lenguaje de
-programación JavaScript. El usurio puede hacer click en el botón editar,
-el cual abre una ventana emergente con un formulario donde puede escribir
-su nombre y una descripción que puede guardar.
+Programación de JavaScript:
 
-Pronto esta página web podrá ser cien por ciento aditable, donde el usuario
-seleccionará su foto de avatar y agregará sus porpias fotografías en las
-tarjetas que las describen, las cuales recibirán los "me gusta" de las
-personas que las aprecien a través de una vista previa.
+Perfil editable donde el usuario puede hacer click en el botón editar,
+escribir su nombre y descripción para guardarlos como título
+principal de la página.
+
+Una serie de seis tarjetas principales al cargar la página, que mediante
+su función y método se añaden al final. Un array contiene las propiedades
+de nombre y enlace correspondientes para cada una de las tarjetas.
+
+El usuario puede agregar sus propias tarjetas personalizadas a través de un
+formulario que se abre al hacer click en el botón añadir. Cada una de las
+tarjetas que cree el usuario se irán agregando al inicio de la serie; utilizando
+las variables y método correspondiente para ello.
+
+El método addEventListener con el evento “click” aplicado en el corazón de las
+tarjetas, permite que el usuario pueda dar “Me gusta” a cada una de ellas. El
+efecto aparece y desaparece gracias al método toggle que agrega y quita
+la clase que pinta el corazón.
+
+Las imágenes de las tarjetas pueden ser visualizadas al hacer click en ellas,
+aparece una ventana emergente que muestra en plenitud la fotografía con su nombre.
+Estas imágenes son adaptativas.
+
+Una serie de variables, funciones, métodos y propiedades hacen posible
+la interactividad de esta página web.
 
 https://charleandresb.github.io/web_project_around/
--- scripts/index.js
+++ scripts/index.js
@@ -6,8 +6,22 @@ const profileName = document.querySelector(".profile__info-name");
 const profileAbout = document.querySelector(".profile__info-description");
 const inputName = document.querySelector(".popup__input-name");
 const inputAbout = document.querySelector(".popup__input-about");
+//formulario editar perfil ^^
+const openCardFormButton = document.querySelector(".profile__add-button");
+const closeCardFormButton = document.querySelector(".popup-add__close-icon");
+const popupAdd = document.querySelector(".popup-add");
+const cardForm = document.querySelector(".popup-add__form");
+const inputCardName = document.querySelector(".popup-add__input-name");
+const inputCardLink = document.querySelector(".popup-add__input-link");
+//formulario agregar cartas ^^
+const popupImage = document.querySelector(".popup-image__container");
+const closePopupImage = document.querySelector(".popup-image__close-icon");
+const popupImageCard = document.querySelector(".popup-image__card");
+const popupImageTitle = document.querySelector(".popup-image__title");
+//popup imagen cartas ^^
 const template = document.querySelector(".template-card");
 const cardSpace = document.querySelector(".photo-cards");
+//template cartas ^^
 
 const initialCards = [
   {
@@ -36,18 +50,33 @@ const initialCards = [
   },
 ];
 
-function cardMaker(name, link) {
+function makeCards(name, link) {
   const card = template.cloneNode(true).content.querySelector(".card");
   const cardTitle = card.querySelector(".card__title");
   const cardImage = card.querySelector(".card__image");
+  const cardTrash = card.querySelector(".card__image-trash");
+  const cardLike = card.querySelector(".card__image-like");
+  cardTrash.addEventListener("click", function () {
+    card.remove();
+  });
+  cardLike.addEventListener("click", function () {
+    cardLike.classList.toggle("card__image-like_active");
+  });
+  cardImage.addEventListener("click", function () {
+    popupImage.classList.add("popup-image__container_opened");
+    popupImageCard.src = link;
+    popupImageTitle.textContent = name;
+    popupImageCard.alt = name;
+  });
   cardTitle.textContent = name;
   cardImage.src = link;
   cardImage.alt = name;
-  cardSpace.append(card);
+  return card;
 }
 
 initialCards.forEach(function (item) {
-  cardMaker(item.name, item.link);
+  const cardToEnd = makeCards(item.name, item.link);
+  cardSpace.append(cardToEnd);
 });
 
 function toggleForm(evt) {
@@ -68,4 +97,26 @@ formProfile.addEventListener("submit", function (evt) {
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
   handleCloseProfile();
+});
+
+function toggleCardForm(evt) {
+  popupAdd.classList.toggle("popup-add_opened");
+}
+
+function handleCloseCardForm() {
+  popupAdd.classList.add("popup-add_opened");
+}
+
+openCardFormButton.addEventListener("click", toggleCardForm);
+closeCardFormButton.addEventListener("click", toggleCardForm);
+
+cardForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  const cardToStar = makeCards(inputCardName.value, inputCardLink.value);
+  cardSpace.prepend(cardToStar);
+  handleCloseCardForm();
+});
+
+closePopupImage.addEventListener("click", function () {
+  popupImage.classList.remove("popup-image__container_opened");
 });
